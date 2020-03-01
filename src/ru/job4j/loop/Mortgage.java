6412b732d404ec716860720b920830292611684b
Makigen <makige.ee@mail.ru>
2020-03-01 23:23:34 +0200

    5.7. Ипотека.[#244447]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double total = amount + (amount * percent / 100);
+        while (total > 0) {
+            year++;
+            total = (total - salary)* percent / 100 + total - salary;
+        }
+        return year;
+    }
+}
