9656f390a70b354759b9fe2c15a3f5c61741cb9e
Makigen <makige.ee@mail.ru>
2020-03-04 23:15:24 +0200

    6.0.2. Заполнение массива.[#244393]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,6 +4,16 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Kirill Malikov";
+        names[2] = "Vladimir Putin";
+        names[3] = "Kersti Kaljulaid";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
