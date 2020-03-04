c1b909f9876c5fa53f6a545622b1defda5b3e414
Makigen <makige.ee@mail.ru>
2020-03-05 01:35:55 +0200

    6.5.3. Сортировка выборкой.[#244387]

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int j = 0; j < data.length; j++) {
+        int min = MinDiapason.findMin(data, j, data.length-1);
+        int index = FindLoop.indexOf(data, min, j, data.length-1);
+        SwitchArray.swap(data, index, j);
+        }
+        return data;
+    }
+}
