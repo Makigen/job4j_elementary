049ec8728e7a7e6101833065ce48b8a8c29a0b14
Makigen <makige.ee@mail.ru>
2020-03-05 15:21:49 +0200

    Упрощено до одного if

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,11 +4,11 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            if (data[i] == data[0]) result = true;
-            else {
+            if (data[i] != data[0]) {
                 result = false;
                 break;
             }
-        } return result;
+        }
+        return result;
     }
 }
