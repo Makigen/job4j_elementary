cd252c00f880fd1d0fa8162a1291a6081d72d366
Makigen <makige.ee@mail.ru>
2020-03-07 18:40:01 +0200

    Объединить два массива [#124482]

---
+++ src/ru/job4j/sort/Merge.java
@@ -0,0 +1,44 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0; //left array index;
+        int j = 0; //right array index;
+        int k = 0; //rsl array index;
+        while (k < rsl.length && i < left.length && j < right.length) {
+            if (left[i] >= right[j]) {
+                rsl[k] = right[j];
+                j++;
+                k++;
+            } else if (left[i] < right[j]) {
+                rsl[k] = left[i];
+                i++;
+                k++;
+            }
+        } //code below is executed when one of the initial arrays is ended
+        while (k < rsl.length && i < left.length) {
+            rsl[k] = left[i];
+            i++;
+            k++;
+        }
+        while (k < rsl.length && j < right.length) {
+            rsl[k] = right[j];
+            j++;
+            k++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
